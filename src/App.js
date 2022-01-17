4ebe85f88c6c587d21edc2f65f78505505fb0853
Tejasvee Dighe <tejasveedighe@gmail.com>
2022-01-17 14:20:40 +0530

    Markdown Editor

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import { Remarkable } from 'remarkable';
+
+function Markdown (props) {
+  let md = new Remarkable();
+  let text = {__html : md.render(props.markdown)}
+  return (
+    <div className='markdown-area' dangerouslySetInnerHTML={text}>
+    </div>
+  );
+}
 
 function App() {
+  const [markdown, setMarkdown] = useState("Hello! **World**")
+  const handleChange = (event) => {
+    setMarkdown(event.target.value);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Input</h1>
+      Enter Some markdown
+      <br />
+      <textarea onChange={handleChange} value={markdown}></textarea>
+      <br/><br/>
+      <h1>Output</h1><br/>
+      <Markdown markdown={markdown}/>
     </div>
   );
 }
